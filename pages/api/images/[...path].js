33a5bd661a48e5ec9fd8c76167f79c9de85b0db1
Hieu Nguyen <hieunguyenel@gmail.com>
2023-03-07 15:17:28 +0700

    update script to store image in folder structure url

--- pages/api/images/[...path].js
+++ pages/api/images/[...path].js
@@ -1,39 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs'
 import path from 'path'
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    const {path: imageFile} = req.query;
+  const { path: imageFile } = req.query;
   const name = imageFile[imageFile.length - 1];
-  
-  let url = 'https://res.cloudinary.com/' + imageFile.join('/');
+  const filePath = path.resolve('.', 'public/images/', ...imageFile);
 
-  try {
-    fs.readFileSync(path.resolve('.', 'public/images/' + name))
-  } catch (error) {
+ let url = 'https://res.cloudinary.com/' + imageFile.join('/');
+ 
+  if (!fs.existsSync(filePath)) {
     console.log('downloading');
-    await download_image(url, 'public/images/'+name);
+    await downloadImage(url, filePath);
   }
-  
-  const filePath = path.resolve('.', 'public/images/' + name)
+
   const imageBuffer = fs.readFileSync(filePath)
 
   res.setHeader('Content-Type', 'image/' + name.substring(name.length - 3))
   res.send(imageBuffer)
-  }
-  
+}
 
-  const download_image = (url, image_path) =>
-  axios({
+const downloadImage = async (url, filePath) => {
+  const response = await axios({
     url,
     responseType: 'stream',
-  }).then(
-    response =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(image_path))
-          .on('finish', () => resolve())
-          .on('error', e => reject(e));
-      }),
-  );
+  });
+
+  const directoryPath = path.dirname(filePath);
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  }
+
+  return new Promise((resolve, reject) => {
+    const stream = response.data.pipe(fs.createWriteStream(filePath));
+    stream.on('finish', () => resolve());
+    stream.on('error', e => reject(e));
+  });
+};
